fc42174ffb3cd1663f0145e262b5377c47d470da
Ksenia Barsukova <kbursukova.02@gmail.com>
2025-09-30 00:08:34 +0300

    Added comparing, ex4.2

--- netology__oop.py
+++ netology__oop.py
@@ -30,6 +30,7 @@ class Student:
         for v in self.grades.values():
             sum += v
             i += 1
+
         srznach = sum // i
         return srznach
 
@@ -40,6 +41,13 @@ class Student:
 Курсы в процессе изучения: {",".join(self.courses_in_progress)}
 Завершенные курсы: Введение в программирование {",".join(self.finished_courses)}"""
 
+    def __cmp__(self, other):
+        if self.val(self.grades)< other.val(other.grades):
+            return -1
+        elif self.val(self.grades)> other.val(other.grades):
+            return 1
+        else:
+            return 0
 class Mentor:
 
     def __init__(self, name, surname):
@@ -68,6 +76,13 @@ class Lecturer(Mentor):
 Фамилия: {self.surname}
 Средняя оценка за лекции: {self.value(self.grades)}
 """
+    def __cmp__(self, other):
+        if self.value(self.grades)< other.value(other.grades):
+            return -1
+        elif self.value(self.grades)> other.value(other.grades):
+            return 1
+        else:
+            return 0
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -90,10 +105,17 @@ print(lecturer.courses_attached)    # []
 print(reviewer.courses_attached)    # []
 
 lecturer = Lecturer('Иван', 'Иванов')
+lecturer_2 = Lecturer('Андрей', 'Козлов')
+lecturer_2.courses_attached += ['Python', 'C++']
+
 reviewer = Reviewer('Пётр', 'Петров')
 student = Student('Алёхина', 'Ольга', 'Ж')
+student_2 = Student('Алёхин', 'Лев', 'М')
+
 
 student.courses_in_progress += ['Python', 'Java']
+student_2.courses_in_progress += ['Python', 'Java', 'C++']
+
 lecturer.courses_attached += ['Python', 'C++']
 reviewer.courses_attached += ['Python', 'C++']
 
@@ -101,8 +123,13 @@ print(student.marks(lecturer, 'Python', 7))  # None
 print(student.marks(lecturer, 'Java', 8))  # Ошибка
 print(student.marks(lecturer, 'С++', 8))  # Ошибка
 print(student.marks(reviewer, 'Python', 6))  # Ошибка
+print(student_2.marks(reviewer, 'C++', 9))  # Ошибка
+
+print(student.marks(lecturer_2, 'Python', 5))  # None
 
 print(lecturer.grades)  # {'Python': [7]}
 print(lecturer)
 print(reviewer)
 print(student)
+print(lecturer.__cmp__(lecturer_2))
+print(student.__cmp__(student_2))
